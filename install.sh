19c5905da749d3589b21adb9fe76385508e88a13
Li_Mengxun <limengxun45@outlook.com>
2022-05-12 23:35:44 +0800

    add an optional service file.

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/bash
 
 THIS_DIR=$(dirname $BASH_SOURCE)
+ROOT_CONF=/etc/.connectPorts_conf.json
+
 sudo cp $THIS_DIR/connectPorts.py /usr/bin/connectPorts
 sudo chmod a+x /usr/bin/connectPorts
-sudo cp $THIS_DIR/.connectPorts_conf.json /etc/.connectPorts_conf.json
+
+if [ ! -f $ROOT_CONF ]
+then
+    sudo cp $THIS_DIR/.connectPorts_conf.json $ROOT_CONF
+else
+    # to prevent overwrite ROOT_CONF
+    echo "$ROOT_CONF exists & will not be updated, uninstall before running this script."
+fi
+
+# sudo cp $THIS_DIR/connectBasePort.service /etc/systemd/system/
+# sudo chmod u+x /etc/systemd/system/connectBasePort.service
+
 echo "Finished."
--- uninstall.sh
+++ uninstall.sh
@@ -2,4 +2,7 @@
 
 sudo rm /usr/bin/connectPorts
 sudo rm /etc/.connectPorts_conf.json
+
+# sudo rm /etc/systemd/system/connectBasePort.service
+
 echo "Finished."
